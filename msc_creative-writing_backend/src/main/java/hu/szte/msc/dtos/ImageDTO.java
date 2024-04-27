015559c65dabfcccaa8e5586bc2d0f958dfa7733
Márta Petra <h984430@stud.u-szeged.hu>
2024-04-27 14:10:53 +0200

    Added Image and mageDTO objects

---
+++ msc_creative-writing_backend/src/main/java/hu/szte/msc/dtos/ImageDTO.java
@@ -0,0 +1,5 @@
+package hu.szte.msc.dtos;
+
+public record ImageDTO(String fileName, String imageUrl) {
+    
+}
---
+++ msc_creative-writing_backend/src/main/java/hu/szte/msc/entities/Image.java
@@ -0,0 +1,27 @@
+package hu.szte.msc.entities;
+
+public class Image {
+    private String fileName;
+    private String url;
+    public String getFileName() {
+        return fileName;
+    }
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    public String getUrl() {
+        return url;
+    }
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    public Image() {
+    }
+    public Image(String fileName, String url) {
+        this.fileName = fileName;
+        this.url = url;
+    }
+
+    
+    
+}
